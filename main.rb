d258f1a83d438173cf46291093968c9e1b936991
mont-belln <yihy1vk2kd3.0120110715@i.softbank.jp>
2015-04-04 11:16:40 +0900

    change function

--- main.rb
+++ main.rb
@@ -1,7 +1,18 @@
 require 'sinatra'
+require 'sqlite3'
+require 'pp'
+
+db = SQLite3::Database.new "db/post.db"
+db.results_as_hash = true
+
 
 get '/' do
-  erb :index
+  posts = db.execute("SELECT * FROM posts ORDER BY id DESC")
+
+  #pp posts
+
+  #erb :index
+  erb :index,{ :locals => { :posts => posts}}
 end
 
 get '/hello' do
@@ -12,5 +23,11 @@ get '/example' do
   erb :example
 end
 post '/' do
-  p params["ex_text"]
+#  p params["ex_text"]
+stmt = db.prepare("INSERT INTO posts (text)
+VALUES(?)")
+
+stmt.bind_params(params["ex_text"])
+stmt.execute
+redirect '/'
 end
