0c7bf3bca8b28105a765a17b2df1ce60d82245c0
mont-belln <yihy1vk2kd3.0120110715@i.softbank.jp>
2015-04-04 11:52:56 +0900

    change function

--- main.rb
+++ main.rb
@@ -1,6 +1,10 @@
+# -*- coding: utf-8 -*-
+#encording: utf-8
+
 require 'sinatra'
 require 'sqlite3'
-require 'pp'
+#require 'pp'
+require 'securerandom'
 
 db = SQLite3::Database.new "db/post.db"
 db.results_as_hash = true
@@ -24,10 +28,35 @@ get '/example' do
 end
 post '/' do
 #  p params["ex_text"]
-stmt = db.prepare("INSERT INTO posts (text)
-VALUES(?)")
+#stmt = db.prepare("INSERT INTO posts (text)
+#VALUES(?)")
+#stmt.bind_params(params["ex_text"])
+file_name = ""
 
-stmt.bind_params(params["ex_text"])
-stmt.execute
-redirect '/'
-end
+  if params["file"]
+    ext = ""
+    if params["file"][:type].include? "jpeg"
+      ext = "jpg"
+    elsif params["file"][:type].include? "png"
+      ext = "png"
+    else
+      return "投稿できる画像形式はjpgとpngだけです"
+    end
+    
+    #適当なファイル名をつける
+    file_name = SecureRandom.hex + "." + ext
+
+    #画像を保存
+    File.open("./public/uploads/" + file_name,'wb')do |f|
+      f.write params["file"][:tempfile].read
+    end
+  else
+    return "画像が必須です"
+  end
+
+  stmt = db.prepare("INSERT INTO posts (text,img_file_name) VALUES (?, ?)")
+  stmt.bind_params(params["ex_text"],file_name)
+      stmt.execute
+      redirect '/'
+    end
+    
